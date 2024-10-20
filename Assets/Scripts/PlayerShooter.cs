e45d490aec87c106e762ce3cef8afe77863826d3
CBowneUCF <156275809+CBowneUCF@users.noreply.github.com>
2024-10-20 16:50:24 -0400

    Player Shoot Update

--- Assets/Scripts/PlayerShooter.cs
+++ Assets/Scripts/PlayerShooter.cs
@@ -6,28 +6,28 @@ public class PlayerShooter : MonoBehaviour
 {
     public Vector2 mouseMovementScale = Vector2.one;
     public ObjectPool Pool;
-
+    new Camera camera;
 
     private void Awake()
     {
         Pool = GetComponent<ObjectPool>();
-        Cursor.lockState = CursorLockMode.Locked; 
+        camera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 mouse = new(Input.GetAxis("Mouse X") * mouseMovementScale.x, Input.GetAxis("Mouse Y") * mouseMovementScale.y);
-        transform.eulerAngles += new Vector3(-mouse.y, mouse.x, 0);
-
+        //Vector2 mouse = new(Input.GetAxis("Mouse X") * mouseMovementScale.x, Input.GetAxis("Mouse Y") * mouseMovementScale.y);
+        //transform.eulerAngles += new Vector3(-mouse.y, mouse.x, 0);
 
         if (Input.GetMouseButtonDown(0))
         {
+            Ray screenRay = camera.ScreenPointToRay(Input.mousePosition);
             GameObject bullet = Pool.Pump();
             if(bullet is not null)
             {
                 bullet.transform.position = transform.position;
-                bullet.transform.eulerAngles = transform.eulerAngles;
+                bullet.transform.rotation = Quaternion.LookRotation(screenRay.direction);
                 bullet.GetComponent<Bullet>().Activate();
             }
         }
